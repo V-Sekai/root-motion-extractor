f910659d3e4e499b413b59af0a230b56bcd0fc85
Saracen <SaracenOne@gmail.com>
2021-03-11 03:24:18 +0000

    Update template syntax

--- root_motion_extractor_template.gd
+++ root_motion_extractor_template.gd
@@ -8,9 +8,16 @@ func post_import(p_scene: Object) -> Object:
 	var source_file_path: String = get_source_file()
 	p_scene = RootMotionExtractor.root_motion_import_function(source_file_path, p_scene,
 	{
-		"Run":{},
-		"Walk":{},
-		"Idle":{}
+		"Run": 
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ORIGIN_X | 
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ORIGIN_Z,
+		"BaseballPitch":
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ORIGIN_X | 
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ORIGIN_Z,
+		"ChangeDirection":
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ORIGIN_X | 
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ORIGIN_Z |
+			RootMotionExtractor.root_motion_flags_const.EXTRACT_ROTATION_Y,
 	})
 	
 	return p_scene
